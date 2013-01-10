a6930ad91992e1bded082198d2a70712377dd2f7
Grégoire HUBERT <hubert.greg@gmail.com>
2013-01-10 13:12:45 +0000

    Changes result iterator.

--- lib/Obdmong/ResultIterator.php
+++ lib/Obdmong/ResultIterator.php
@@ -1,21 +1,47 @@
 <?php
 namespace Obdmong;
 
-class ResultIterator extends \IteratorIterator
+class ResultIterator implements \OuterIterator
 {
     protected $map;
+    protected $cursor;
 
     public function __construct(\MongoCursor $mongo_cursor, EntityMap $entity_map)
     {
         $this->map = $entity_map;
-        parent::__construct($mongo_cursor);
+        $this->cursor = $mongo_cursor;
+    }
+
+    public function getInnerIterator()
+    {
+        return $this->cursor;
     }
 
     public function current()
     {
         $entity_class = $this->map->getEntityClass();
 
-        return new $entity_class(parent::current());
+        return new $entity_class($this->getInnerIterator()->current());
+    }
+
+    public function next()
+    {
+        $this->getInnerIterator()->next();
+    }
+
+    public function rewind()
+    {
+        $this->getInnerIterator()->rewind();
+    }
+
+    public function key()
+    {
+        return $this->getInnerIterator()->key();
+    }
+
+    public function valid()
+    {
+        return $this->getInnerIterator()->valid();
     }
 
     public function export()
